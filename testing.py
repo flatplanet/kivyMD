b1356573d879341cf9c1cca4f88eb2e3bc85234f
John Elder <johne4196@gmail.com>
2021-03-01 09:45:18 -0800

    initial commit

---
+++ testing.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+# Designate Our .kv design file 
+Builder.load_file('testing.kv')
+
+class MyLayout(Widget):
+	pass
+
+class AwesomeApp(MDApp):
+	def build(self):
+		return MyLayout()
+
+
+if __name__ == '__main__':
+	AwesomeApp().run()
+
+
+
