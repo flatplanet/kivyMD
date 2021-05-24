f3727113dfb46351b342ff7b29b7fc22bc4860dd
John Elder <johne4196@gmail.com>
2021-05-24 08:57:58 -0700

    image swiper

---
+++ swiper.py
@@ -0,0 +1,13 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp):
+
+	def build(self):
+		self.theme_cls.theme_style = "Dark"
+		self.theme_cls.primary_palette = "BlueGray"
+		return Builder.load_file('swiper.kv')
+	
+	
+MainApp().run()
